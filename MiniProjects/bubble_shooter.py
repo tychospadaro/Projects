a71e0350bd0d732a0475ea56f422e781851c9bb3
Tycho Spadaro <tycho.spadaro@gmail.com>
2018-04-04 04:23:55 +0100

    completed bubbleshooter, stable in codeskukptor

--- MiniProjects/bubble_shooter.py
+++ MiniProjects/bubble_shooter.py
@@ -15,12 +15,9 @@ COLOR_LIST = ["Red", "Green", "Blue", "White"]
 BUBBLE_SPD = 0.2
 FIRING_SOUND = simplegui.load_sound("http://commondatastorage.googleapis.com/codeskulptor-assets/Collision8-Bit.ogg")
 
-
 # global variables
 firing_angle = math.pi / 2
 firing_angle_vel = 0
-bubble_stuck = True
-
 
 # helper functions to handle transformations
 def angle_to_vector(ang):
@@ -31,33 +28,46 @@ def dist(p,q):
 
 # class defintion for Bubbles
 class Bubble:
-
     def __init__(self):
         self.pos = FIRING_POSITION
         self.vel = [0,0]
         self.color = random.choice(COLOR_LIST)
         self.radius = BUBBLE_RADIUS
         self.sound = FIRING_SOUND
+        self.fired = False
 
+    def draw(self, canvas):
+        canvas.draw_circle(self.pos, self.radius, 1,
+                           self.color, self.color)
     def update(self):
-        self.pos = [self.pos[0] + self.vel[0],
-                    self.pos[1] + self.vel[1]]
+        if self.vel == [0, 0]:				# stopped
+            return
+        elif self.pos[1] + self.vel[0] <= BUBBLE_RADIUS:	# hit top
+            self.vel = [0,0]
+            self.pos[1] = self.radius
+            return
+        else:								# move, then check side
+            self.pos = [self.pos[0] + self.vel[0],
+                        self.pos[1] + self.vel[1]]
         if not(self.radius < self.pos[0] < WIDTH - self.radius):
             self.vel[0] *= -1
 
     def fire_bubble(self, vel):
         self.vel = vel
+        self.fired = True
         self.sound.play()
 
-    def is_stuck(self):
-        pass
-
     def collide(self, bubble):
-        pass
+        distance = dist(self.pos, bubble.pos)
+        if distance <= 2 * self.radius:
+            self.vel = [0,0]
+            return True
+        else:
+            return False
 
-    def draw(self, canvas):
-        canvas.draw_circle(self.pos, self.radius, 1,
-                           self.color, self.color)
+
+    def is_fired(self):
+        return self.fired
 
 # a class definition for the shooter
 class Shooter:
@@ -68,9 +78,25 @@ class Shooter:
         self.anchor = FIRING_POSITION
         self.left = False
         self.right = False
-        self.get_tip()
-        self.bubbles = [Bubble()]
-        self.is_loaded = True
+        self.active_bubble = Bubble()
+        self.stuck_bubbles = set()
+
+    def draw(self, canvas):
+        canvas.draw_line(self.tip, self.anchor, 2, 'lime')
+        self.active_bubble.draw(canvas)
+        map(lambda bubble: bubble.draw(canvas), self.stuck_bubbles)
+
+    def update(self):
+        self.angle_vel = (int(self.left) - int(self.right))\
+                         * FIRING_ANGLE_VEL_INC
+        new_angle = self.angle + self.angle_vel
+        if 0 < new_angle < math.pi:
+            self.angle = new_angle
+            self.get_tip()
+        if self.active_bubble.is_fired():
+            self.active_bubble.update()
+            map(lambda stuck: self.active_bubble.collide(stuck), self.stuck_bubbles)
+            self.reload_bubble()
 
     def get_tip(self):
         angle_vector = angle_to_vector(self.angle)
@@ -83,25 +109,16 @@ class Shooter:
     def move_right(self):
         self.right = not self.right
 
-    def update(self):
-        self.angle_vel = (int(self.left) - int(self.right))\
-                         * FIRING_ANGLE_VEL_INC
-        new_angle = self.angle + self.angle_vel
-        if 0 < new_angle < math.pi:
-            self.angle = new_angle
-            self.get_tip()
-
-        map(lambda bubble: bubble.update(), self.bubbles)
-
     def launch(self):
-        if self.is_loaded:
+        if self.active_bubble.fired == False:
             fire_vel = [(self.tip[0] - self.anchor[0]) * BUBBLE_SPD,
                         (self.tip[1] - self.anchor[1]) * BUBBLE_SPD]
-            self.bubbles[-1].fire_bubble(fire_vel)
+            self.active_bubble.fire_bubble(fire_vel)
 
-    def draw(self, canvas):
-        canvas.draw_line(self.tip, self.anchor, 2, 'lime')
-        map(lambda bubble: bubble.draw(canvas), self.bubbles)
+    def reload_bubble(self):
+        if self.active_bubble.vel == [0, 0]:
+            self.stuck_bubbles.add(self.active_bubble)
+            self.active_bubble = Bubble()
 
 the_shooter = Shooter()
 
@@ -141,6 +158,8 @@ def draw(canvas):
 
 
     # update a_bubble and check for sticking
+
+
 
     # draw a bubble and stuck bubbles
 
